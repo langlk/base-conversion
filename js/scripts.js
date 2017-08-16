9c433cb00488d5ab388f9bcd652aff823ee9fb46
Kelsey Langlois and Luciano Oliveira <luciano@>
2017-08-16 14:35:27 -0700

    update binaryConvert to convert all numbers larger than 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 function binaryConvert(decimalNum) {
-  if (decimalNum === 1) {
-    return 1;
+  if (decimalNum > 1) {
+    var binaryString = "";
+    while (decimalNum > 1) {
+      binaryString = decimalNum % 2 + binaryString;
+      decimalNum = parseInt(decimalNum / 2);
+    }
+    binaryString = 1 + binaryString;
+    return binaryString;
+  } else if (decimalNum === 1) {
+    return "1";
   } else if (decimalNum === 0) {
-    return 0;
+    return "0";
   } else {
     return NaN;
   }
